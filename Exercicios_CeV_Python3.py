bc42e0ac6b09a5e81e96f085dc458f90eb87bafb
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-02 16:29:10 -0300

    Update Exercicios_CeV_Python3.py

--- Exercicios_CeV_Python3.py
+++ Exercicios_CeV_Python3.py
@@ -411,20 +411,20 @@ else:
 #     print("Classificação: MASTER")
 
 # exercicio 042
-r1 = float(input("Primeiro segmento: "))
-r2 = float(input("Segundo segmento: "))
-r3 = float(input("Terceiro segmento: "))
+# r1 = float(input("Primeiro segmento: "))
+# r2 = float(input("Segundo segmento: "))
+# r3 = float(input("Terceiro segmento: "))
 
-if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r3:
-    print("Os segmentos acima PODEM formar um triângulo ", end="")
-    if r1 == r2 == r3:
-        print("EQUILÁTERO")
-    elif r1 != r2 != r3 != r1:
-        print("ESCALENO")
-    else:
-        print("ISÓCELES")
-else:
-    print("Os segmentos acima NÃO podem formar um triângulo")
+# if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r3:
+#     print("Os segmentos acima PODEM formar um triângulo ", end="")
+#     if r1 == r2 == r3:
+#         print("EQUILÁTERO")
+#     elif r1 != r2 != r3 != r1:
+#         print("ESCALENO")
+#     else:
+#         print("ISÓCELES")
+# else:
+#     print("Os segmentos acima NÃO podem formar um triângulo")
 
 # exercicio 043
 # peso = float(input("Qual seu peso? (Kg) "))
