808a1dfc82b16e84c54036e53321dcf79a90c529
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-28 19:58:01 -0300

    Update Exercicios_funcoes.py

--- Exercicios_Python/Exercicios Mundo3/Exercicios_funcoes.py
+++ Exercicios_Python/Exercicios Mundo3/Exercicios_funcoes.py
@@ -166,6 +166,12 @@ os.system("cls")
 
 # exercicio 105
 # def notas (*n, sit=False):
+#     """
+#     -> Função para analisar notas e situações de vários alunos.
+#         - param n: uma ou mais notas dos alunos
+#         - param sit: valor opcional, indicando se deve ou não adicionar a situação
+#         - return: dicionário com várias informações sobre a situação do aluno/turma.
+#     """
 #     dados = dict()
 #     dados["total"] = len(n)
 #     dados["maior"] = max(n)
@@ -181,3 +187,37 @@ os.system("cls")
 #     return dados
 # resp = notas(5.5, 9.5, 10, 6.5, sit=False)
 # print(resp)
+
+# exercicio 106
+# c = ("\033[m",           # 0 -sem cores
+#      "\033[0;30;41m",    # 1 - vermelho
+#      "\033[0;30;42m",    # 2 - verde
+#      "\033[0;30;43m",    # 3 - amarelo
+#      "\033[0;30;44m",    # 4 - azul
+#      "\033[0;30;45m",    # 5 - roxo
+#      "\033[7;30m");      # 6 - branco
+
+# def ajuda(com):
+#     titulo(f"Acessando o manual do comando \"{com}\"", 4)
+#     print(c[6], end="")
+#     help(com)
+#     print(c[0], end="")
+#     time.sleep(2)
+
+# def titulo(msg, cor=0):
+#     tam = len(msg) + 4
+#     print(c[cor], end="")
+#     print("~"*tam)
+#     print(f"{msg}")
+#     print("~"*tam)
+#     print(c[0], end="")
+#     time.sleep(1)
+# comando = ""
+# while True:
+#     titulo("SISTEMA DE AJUDA PyHELP", 2)
+#     comando = str(input("Função ou Biblioteca > "))
+#     if comando.upper() == "FIM":
+#         break
+#     else:
+#         ajuda(comando)
+# titulo("ATÉ LOGO!", 1)
