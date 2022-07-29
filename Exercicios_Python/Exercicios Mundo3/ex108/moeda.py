563807950b437bc72977f798f608a8504eb3ec06
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-29 19:46:04 -0300

    Update moeda.py

--- Exercicios_Python/Exercicios Mundo3/ex108/moeda.py
+++ Exercicios_Python/Exercicios Mundo3/ex108/moeda.py
@@ -1,13 +1,14 @@
-from dataclasses import replace
-
-
 def metade(n=0):
     return n/2
+
 def dobro(n=0):
     return n*2
+
 def aumentar(n=0, p=0):
     return n+(n*p/100)
+
 def diminuir(n=0, p=0):
     return n-(n*p/100)
+
 def moeda(n=0, moeda="R$"):
     return f"{moeda}{n:.2f}".replace(".", ",")
