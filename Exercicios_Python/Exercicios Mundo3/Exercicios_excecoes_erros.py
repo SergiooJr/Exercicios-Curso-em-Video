6849f2d91d475f39e9590620136d5c21aa5d6871
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-29 22:20:24 -0300

    Update Exercicios_excecoes_erros.py

--- Exercicios_Python/Exercicios Mundo3/Exercicios_excecoes_erros.py
+++ Exercicios_Python/Exercicios Mundo3/Exercicios_excecoes_erros.py
@@ -10,5 +10,19 @@ def leiaInt(v):
             print("Entrada de dados interrompida pelo usuário!")
         else:
             return n
-num = leiaInt("Digite um valor: ")
-print(f"O valor digitado foi {num}")
+
+def leiaFloat(v):
+    while True:
+        try:
+            num = float(input(v))
+        except (TypeError, ValueError):
+            print("\033[31mERRO! Por favor digite um número inteiro válido.\033[m")
+            continue
+        except KeyboardInterrupt:
+            print("Entrada de dados interrompida pelo usuário!")
+        else:
+            return num
+
+num = leiaInt("Digite um valor inteiro: ")
+numF = leiaFloat("Digite um valor real: ")
+print(f"O valor inteiro digitado foi {num}, e o real foi {numF}")
